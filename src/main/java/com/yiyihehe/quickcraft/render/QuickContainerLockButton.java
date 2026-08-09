7085562dd7c246f8754a8d41d07dfcc46249603b
yiyihehe <164907479@qq.com>
2026-08-10 03:38:55 +0800

    优化：统一容器锁图标和悬浮提示

---
+++ src/main/java/com/yiyihehe/quickcraft/render/QuickContainerLockButton.java
@@ -0,0 +1,31 @@
+package com.yiyihehe.quickcraft.render;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.function.BooleanSupplier;
+
+/** Icon-only button for locking or unlocking the entire current container. */
+public final class QuickContainerLockButton extends ButtonWidget {
+    private static final Identifier LOCK_ICON = Identifier.of("quickcraft", "textures/gui/container_lock.png");
+    private static final Identifier UNLOCK_ICON = Identifier.of("quickcraft", "textures/gui/container_unlock.png");
+    private final BooleanSupplier locked;
+
+    public QuickContainerLockButton(int x, int y, PressAction onPress, BooleanSupplier locked, Text tooltip) {
+        super(x, y, 12, 12, Text.empty(), onPress, DEFAULT_NARRATION_SUPPLIER);
+        this.locked = locked;
+        this.setTooltip(Tooltip.of(tooltip));
+    }
+
+    @Override
+    protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.renderWidget(context, mouseX, mouseY, delta);
+        Identifier icon = this.locked.getAsBoolean() ? LOCK_ICON : UNLOCK_ICON;
+        context.drawTexture(RenderLayer::getGuiTextured, icon,
+                this.getX() + 1, this.getY() + 1, 0, 0, 10, 10, 10, 10);
+    }
+}
