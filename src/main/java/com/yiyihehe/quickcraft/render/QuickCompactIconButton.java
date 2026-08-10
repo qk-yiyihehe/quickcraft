268a53b510dab1b01ee2cd6d19620f8008ed8faf
yiyihehe <164907479@qq.com>
2026-08-10 18:44:59 +0800

    样式：重设计容器操作与预览放大按钮

---
+++ src/main/java/com/yiyihehe/quickcraft/render/QuickCompactIconButton.java
@@ -0,0 +1,32 @@
+package com.yiyihehe.quickcraft.render;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.text.Text;
+
+/** Compact icon button used on the title bars of vanilla container screens. */
+public abstract class QuickCompactIconButton extends QuickDraggableButton {
+    public static final int SIZE = 14;
+
+    protected QuickCompactIconButton(int x, int y, Text label, PressAction onPress, PositionKey positionKey) {
+        super(x, y, SIZE, SIZE, label, onPress, positionKey);
+        this.setTooltip(Tooltip.of(label));
+    }
+
+    @Override
+    protected final void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+        boolean hovered = this.isMouseOver(mouseX, mouseY);
+        int surfaceColor = this.isPositionDragging()
+                ? 0xE04A6F8F
+                : this.getSurfaceColor(hovered);
+        context.fill(this.getX() + 1, this.getY() + 1, this.getX() + SIZE, this.getY() + SIZE, 0x70000000);
+        context.fill(this.getX(), this.getY(), this.getX() + SIZE - 1, this.getY() + SIZE - 1, surfaceColor);
+        this.drawIcon(context, this.getX() + 2, this.getY() + 2, hovered);
+    }
+
+    protected int getSurfaceColor(boolean hovered) {
+        return hovered ? 0xE0525252 : 0xC0323232;
+    }
+
+    protected abstract void drawIcon(DrawContext context, int x, int y, boolean hovered);
+}
--- src/main/java/com/yiyihehe/quickcraft/render/QuickContainerLockButton.java
+++ src/main/java/com/yiyihehe/quickcraft/render/QuickContainerLockButton.java
@@ -1,29 +1,34 @@
 package com.yiyihehe.quickcraft.render;
 
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.function.BooleanSupplier;
 
 /** Icon-only button for locking or unlocking the entire current container. */
-public final class QuickContainerLockButton extends QuickDraggableButton {
+public final class QuickContainerLockButton extends QuickCompactIconButton {
     private static final Identifier LOCK_ICON = Identifier.of("quickcraft", "textures/gui/container_lock.png");
     private static final Identifier UNLOCK_ICON = Identifier.of("quickcraft", "textures/gui/container_unlock.png");
     private final BooleanSupplier locked;
 
     public QuickContainerLockButton(int x, int y, PressAction onPress, BooleanSupplier locked,
                                     PositionKey positionKey, Text tooltip) {
-        super(x, y, 12, 12, Text.empty(), onPress, positionKey);
+        super(x, y, tooltip, onPress, positionKey);
         this.locked = locked;
-        this.setTooltip(Tooltip.of(tooltip));
     }
 
     @Override
-    protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.renderWidget(context, mouseX, mouseY, delta);
+    protected int getSurfaceColor(boolean hovered) {
+        if (this.locked.getAsBoolean()) {
+            return hovered ? 0xE0786740 : 0xC05C4C30;
+        }
+        return super.getSurfaceColor(hovered);
+    }
+
+    @Override
+    protected void drawIcon(DrawContext context, int x, int y, boolean hovered) {
         Identifier icon = this.locked.getAsBoolean() ? LOCK_ICON : UNLOCK_ICON;
-        context.drawTexture(icon, this.getX() + 1, this.getY() + 1, 0, 0, 10, 10, 10, 10);
+        context.drawTexture(icon, x, y, 0, 0, 10, 10, 10, 10);
     }
 }
