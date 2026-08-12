1838275d157ceab9ee94b70c17d36c1b959f0fbd
yiyihehe <164907479@qq.com>
2026-08-13 02:18:30 +0800

    新功能：支持自定义投影预览图

--- src/main/java/com/yiyihehe/quickcraft/config/QuickCraftConfigs.java
+++ src/main/java/com/yiyihehe/quickcraft/config/QuickCraftConfigs.java
@@ -337,6 +337,14 @@ public final class QuickCraftConfigs implements IConfigHandler {
                 true,
                 ""
         ).apply(PROJECTION_TRANSLATION_PREFIX);
+        public static final ConfigBoolean ALLOW_ADDING_LITEMATICA_PREVIEW_IMAGES = new ConfigBoolean(
+                "allowAddingLitematicaPreviewImages",
+                true
+        ).apply(PROJECTION_TRANSLATION_PREFIX);
+        public static final ConfigBoolean REPLACE_LITEMATICA_PREVIEW_WITH_3D = new ConfigBoolean(
+                "replaceLitematicaPreviewWith3D",
+                true
+        ).apply(PROJECTION_TRANSLATION_PREFIX);
         public static final ConfigBooleanHotkeyed ALLOW_EASY_PLACE_VANILLA_INTERACTIONS = new ConfigBooleanHotkeyed(
                 "allowEasyPlaceOpenContainers",
                 false,
@@ -496,6 +504,8 @@ public final class QuickCraftConfigs implements IConfigHandler {
         ).apply(PROJECTION_TRANSLATION_PREFIX);
         public static final List<IConfigBase> OPTIONS = List.of(
                 SHOW_LITEMATICA_3D_PREVIEW,
+                ALLOW_ADDING_LITEMATICA_PREVIEW_IMAGES,
+                REPLACE_LITEMATICA_PREVIEW_WITH_3D,
                 ALLOW_EASY_PLACE_VANILLA_INTERACTIONS,
                 ALLOW_EASY_PLACE_INTERACTION_SCREENS,
                 ALLOW_EASY_PLACE_REDSTONE_INTERACTIONS,
@@ -968,6 +978,14 @@ public final class QuickCraftConfigs implements IConfigHandler {
 
     public static boolean isLitematica3DPreviewEnabled() {
         return ProjectionTools.SHOW_LITEMATICA_3D_PREVIEW.getBooleanValue();
+    }
+
+    public static boolean canAddLitematicaPreviewImages() {
+        return ProjectionTools.ALLOW_ADDING_LITEMATICA_PREVIEW_IMAGES.getBooleanValue();
+    }
+
+    public static boolean shouldReplaceLitematicaPreviewWith3D() {
+        return ProjectionTools.REPLACE_LITEMATICA_PREVIEW_WITH_3D.getBooleanValue();
     }
 
     public static boolean areEasyPlaceVanillaInteractionsAllowed() {
