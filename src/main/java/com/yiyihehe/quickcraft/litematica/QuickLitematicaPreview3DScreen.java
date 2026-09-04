c22ac2d2b5c2b54775bfc3cdb353d72f776f0e07
yiyihehe <164907479@qq.com>
2026-09-04 12:11:23 +0800

    修复：全屏选图对话框不可见并保持窗口最大化

--- src/main/java/com/yiyihehe/quickcraft/litematica/QuickLitematicaPreview3DScreen.java
+++ src/main/java/com/yiyihehe/quickcraft/litematica/QuickLitematicaPreview3DScreen.java
@@ -434,7 +434,14 @@ public final class QuickLitematicaPreview3DScreen extends Screen {
     }
 
     @Override
+    public void tick() {
+        super.tick();
+        this.manager.pollPendingPreviewImagePicker();
+    }
+
+    @Override
     public void close() {
+        this.manager.cancelPendingPreviewImagePicker();
         if (this.closeManagerOnExit) {
             this.manager.close();
         }
