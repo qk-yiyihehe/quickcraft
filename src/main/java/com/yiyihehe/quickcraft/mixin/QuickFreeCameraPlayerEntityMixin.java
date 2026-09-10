8c8c766ab6a0a1f8b1cef726648230f2e5e9c8d6
yiyihehe <164907479@qq.com>
2026-09-10 15:10:02 +0800

    新功能：增强灵魂出窍交互和轻松放置
    (cherry picked from commit b8d84581fca6198dfecb7c543e356524235d2d9e)
    (cherry picked from commit 4661b33dd4d8a0ab6e68a772b96661669e2de8d3)
    (cherry picked from commit bb47db7358bbba6e45a59375c3bd73d45290d752)
    (cherry picked from commit e7923cb554948ae5f7a969f228e230b724ba585d)
    (cherry picked from commit 7a4210be77ef4f1552ec5aab9febac4bdeb8fa85)

--- src/main/java/com/yiyihehe/quickcraft/mixin/QuickFreeCameraPlayerEntityMixin.java
+++ src/main/java/com/yiyihehe/quickcraft/mixin/QuickFreeCameraPlayerEntityMixin.java
@@ -10,15 +10,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * 灵魂出窍增强开启时，按潜行键让客户端按原版规则取消方块交互。
- * Tweakeroo 冻本体后 {@code isSneaking()} 不会跟随潜行键；若此处失效，对着箱子右键仍会开箱而不是放置。
+ * 灵魂出窍增强开启时，按潜行键让客户端按原版规则取消方块或实体的默认交互。
+ * Tweakeroo 冻本体后 {@code isSneaking()} 不会跟随潜行键；若此处失效，箱子和可骑乘实体会执行错误操作。
  */
 @Mixin(PlayerEntity.class)
 public abstract class QuickFreeCameraPlayerEntityMixin {
     @Inject(method = "shouldCancelInteraction", at = @At("HEAD"), cancellable = true)
-    private void quickcraft$sneakPlaceFromFreeCamera(CallbackInfoReturnable<Boolean> cir) {
+    private void quickcraft$cancelInteractionFromFreeCamera(CallbackInfoReturnable<Boolean> cir) {
         if ((Object) this instanceof ClientPlayerEntity
-                && QuickFreeCameraInteractions.shouldSneakPlaceFromFreeCamera(MinecraftClient.getInstance())) {
+                && QuickFreeCameraInteractions.shouldCancelInteractionFromFreeCamera(MinecraftClient.getInstance())) {
             cir.setReturnValue(true);
         }
     }
