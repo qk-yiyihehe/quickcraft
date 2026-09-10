a4e3c923eacebfc02bddd379c5d8ebc271ac9381
yiyihehe <164907479@qq.com>
2026-09-10 17:45:06 +0800

    修复：灵魂出窍方向方块放置
    (cherry picked from commit f339f3e4412f8c6679c6f40d9cfa925a1f082864)
    (cherry picked from commit 2748d750652d0b7eb5c05de7dca78ed6f6d7f908)
    (cherry picked from commit 642748a12ca48ff60175afc8c972eb4a871c57e3)
    (cherry picked from commit b9fdf0c2ac6b1a9c84627ffba4e93ca4cc3c62fb)
    (cherry picked from commit a976b1fd99454e0f74b896e8a2e8472b027a4176)
    (cherry picked from commit 14e228802de020fecca02063f9bef39811d739a7)

--- src/main/java/com/yiyihehe/quickcraft/mixin/QuickFreeCameraMinecraftClientMixin.java
+++ src/main/java/com/yiyihehe/quickcraft/mixin/QuickFreeCameraMinecraftClientMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * 将灵魂视角放置后的服务端朝向恢复延迟到后续客户端 tick。
- * 若与放置包同帧恢复，服务端可能先按本体朝向落块，表现为方块短暂正确后立即回弹。
+ * 若与放置包同帧恢复，服务端可能先按本体朝向落块，表现为方向回弹。
  */
 @Mixin(MinecraftClient.class)
 public abstract class QuickFreeCameraMinecraftClientMixin {
