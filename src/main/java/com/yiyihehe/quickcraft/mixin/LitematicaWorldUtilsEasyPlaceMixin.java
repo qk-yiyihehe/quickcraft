692b9efefcaacee772dee914ce9e3f4ea5b140ee
yiyihehe <164907479@qq.com>
2026-09-10 15:11:48 +0800

    维护：移除冰块轻松放置诊断日志
    (cherry picked from commit 83eed463852686be81209a15b24bdf35efa9a103)
    (cherry picked from commit 48e4c96d39a40138f77e80c491fc9b54bcde4b2e)
    (cherry picked from commit ff5cdb2b211f754333703ae98aa1f015cff14a29)
    (cherry picked from commit 9cb676f358c11cfa377bfbb5babc7e33c2205ed5)
    (cherry picked from commit ca3bc176074f741e509d7bae0c82287f8125086b)
    (cherry picked from commit 9725f25484480196e96a95da3821f90edee85800)

--- src/main/java/com/yiyihehe/quickcraft/mixin/LitematicaWorldUtilsEasyPlaceMixin.java
+++ src/main/java/com/yiyihehe/quickcraft/mixin/LitematicaWorldUtilsEasyPlaceMixin.java
@@ -18,8 +18,6 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -33,18 +31,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(value = WorldUtils.class, remap = false)
 public class LitematicaWorldUtilsEasyPlaceMixin {
-    private static final Logger QUICKCRAFT_EASY_PLACE_LOGGER = LoggerFactory.getLogger("QuickCraft-EasyPlaceIce");
-    private static long quickcraft$lastDiagnosticNanos;
-    private static boolean quickcraft$materialRedirectMatched;
-    private static String quickcraft$iceBranchDiagnostic = "not-run";
-
     @Inject(method = "doEasyPlaceAction", at = @At("HEAD"), cancellable = true, remap = false)
     private static void quickcraft$placeIceOverProjectedSourceWater(
             MinecraftClient client,
             CallbackInfoReturnable<ActionResult> cir
     ) {
-        quickcraft$materialRedirectMatched = false;
-        quickcraft$iceBranchDiagnostic = "preconditions";
         if (!QuickCraftConfigs.isEasyPlaceIceOverSourceWaterAllowed()
                 || client.player == null
                 || client.world == null
@@ -57,7 +48,6 @@ public class LitematicaWorldUtilsEasyPlaceMixin {
         RayTraceUtils.RayTraceWrapper traceWrapper = RayTraceUtils.getGenericTrace(
                 client.world, traceEntity, range, true, true, false
         );
-        quickcraft$iceBranchDiagnostic = "trace=" + (traceWrapper == null ? "none" : traceWrapper.getHitType());
         if (traceWrapper == null
                 || traceWrapper.getHitType() != RayTraceUtils.RayTraceWrapper.HitType.SCHEMATIC_BLOCK) {
             return;
@@ -65,7 +55,6 @@ public class LitematicaWorldUtilsEasyPlaceMixin {
 
         BlockHitResult schematicHit = traceWrapper.getBlockHitResult();
         World schematicWorld = SchematicWorldHandler.getSchematicWorld();
-        quickcraft$iceBranchDiagnostic = "schematic-hit=" + (schematicHit != null) + " world=" + (schematicWorld != null);
         if (schematicHit == null || schematicWorld == null) {
             return;
         }
@@ -73,11 +62,6 @@ public class LitematicaWorldUtilsEasyPlaceMixin {
         BlockPos targetPos = schematicHit.getBlockPos();
         BlockState schematicState = schematicWorld.getBlockState(targetPos);
         BlockState worldState = client.world.getBlockState(targetPos);
-        quickcraft$iceBranchDiagnostic = "target=" + targetPos.toShortString()
-                + " schematic=" + schematicState
-                + " world=" + worldState
-                + " schematicStill=" + schematicState.getFluidState().isStill()
-                + " worldStill=" + worldState.getFluidState().isStill();
         if (!schematicState.isOf(Blocks.WATER)
                 || !schematicState.getFluidState().isStill()
                 || (!worldState.isAir()
@@ -85,11 +69,9 @@ public class LitematicaWorldUtilsEasyPlaceMixin {
             return;
         }
 
-        quickcraft$materialRedirectMatched = true;
         ItemStack iceStack = new ItemStack(Blocks.ICE);
         InventoryUtils.schematicWorldPickBlock(iceStack, targetPos, schematicWorld, client);
         Hand hand = fi.dy.masa.litematica.util.EntityUtils.getUsedHandForItem(client.player, iceStack);
-        quickcraft$iceBranchDiagnostic = "hand=" + hand;
         if (hand == null) {
             cir.setReturnValue(ActionResult.FAIL);
             return;
@@ -104,76 +86,10 @@ public class LitematicaWorldUtilsEasyPlaceMixin {
         }
 
         ActionResult result = client.interactionManager.interactBlock(client.player, hand, placementHit);
-        quickcraft$iceBranchDiagnostic = "interaction=" + result + " hit=" + placementHit.getBlockPos().toShortString()
-                + " side=" + placementHit.getSide();
         if (result.shouldSwingHand()) {
             client.player.swingHand(hand);
         }
         cir.setReturnValue(result == ActionResult.FAIL ? ActionResult.FAIL : ActionResult.SUCCESS);
-    }
-
-    @Inject(method = "doEasyPlaceAction", at = @At("RETURN"), remap = false)
-    private static void quickcraft$logEasyPlaceIceDiagnostic(
-            MinecraftClient client,
-            CallbackInfoReturnable<ActionResult> cir
-    ) {
-        long now = System.nanoTime();
-        if (now - quickcraft$lastDiagnosticNanos < 500_000_000L) {
-            return;
-        }
-        quickcraft$lastDiagnosticNanos = now;
-
-        String target = "none";
-        String worldState = "n/a";
-        String schematicState = "n/a";
-        String schematicTrace = "none";
-        String genericTrace = "none";
-        if (client.crosshairTarget instanceof BlockHitResult hit
-                && hit.getType() == HitResult.Type.BLOCK
-                && client.world != null) {
-            BlockPos pos = hit.getBlockPos();
-            target = pos.toShortString();
-            worldState = client.world.getBlockState(pos).toString();
-            World schematicWorld = SchematicWorldHandler.getSchematicWorld();
-            if (schematicWorld != null) {
-                schematicState = schematicWorld.getBlockState(pos).toString();
-            }
-        }
-
-        if (client.player != null && client.world != null) {
-            Entity traceEntity = QuickFreeCameraInteractions.getEasyPlaceTraceEntity(client, client.player);
-            double range = WorldUtils.getValidBlockRange(client);
-            BlockHitResult schematicHit = RayTraceUtils.traceToSchematicWorld(traceEntity, range, true, true);
-            if (schematicHit != null) {
-                BlockPos pos = schematicHit.getBlockPos();
-                World schematicWorld = SchematicWorldHandler.getSchematicWorld();
-                schematicTrace = pos.toShortString()
-                        + " state=" + (schematicWorld == null ? "n/a" : schematicWorld.getBlockState(pos));
-            }
-            RayTraceUtils.RayTraceWrapper genericHit = RayTraceUtils.getGenericTrace(
-                    client.world, traceEntity, range, true, true, false
-            );
-            if (genericHit != null) {
-                BlockHitResult blockHit = genericHit.getBlockHitResult();
-                genericTrace = genericHit.getHitType()
-                        + (blockHit == null ? "" : " pos=" + blockHit.getBlockPos());
-            }
-        }
-
-        QUICKCRAFT_EASY_PLACE_LOGGER.info(
-                "attempt result={} config={} target={} world={} schematic={} schematicTrace={} genericTrace={} mainHand={} offHand={} materialRedirectMatched={} iceBranch={}",
-                cir.getReturnValue(),
-                QuickCraftConfigs.isEasyPlaceIceOverSourceWaterAllowed(),
-                target,
-                worldState,
-                schematicState,
-                schematicTrace,
-                genericTrace,
-                client.player == null ? "none" : client.player.getMainHandStack().getItem(),
-                client.player == null ? "none" : client.player.getOffHandStack().getItem(),
-                quickcraft$materialRedirectMatched,
-                quickcraft$iceBranchDiagnostic
-        );
     }
 
     @Inject(method = "handleEasyPlace", at = @At("HEAD"), cancellable = true, remap = false)
