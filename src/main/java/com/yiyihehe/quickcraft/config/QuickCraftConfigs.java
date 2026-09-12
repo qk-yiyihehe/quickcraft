953d7a4efd78bd16c48db85f46a185d4b8079886
yiyihehe <164907479@qq.com>
2026-09-12 22:05:00 +0800

    新功能：添加普通合成逐格原料留样
    (cherry picked from commit 3ea4f8735ab5037d1b8e2e5273ab8f5f877b29e3)

--- src/main/java/com/yiyihehe/quickcraft/config/QuickCraftConfigs.java
+++ src/main/java/com/yiyihehe/quickcraft/config/QuickCraftConfigs.java
@@ -261,6 +261,11 @@ public final class QuickCraftConfigs implements IConfigHandler {
                 false,
                 ""
         ).apply(CRAFTING_TRANSLATION_PREFIX);
+        public static final ConfigBooleanHotkeyed RETAIN_ONE_CRAFT_INGREDIENT = new ConfigBooleanHotkeyed(
+                "retainOneCraftIngredient",
+                false,
+                ""
+        ).apply(CRAFTING_TRANSLATION_PREFIX);
 
         public static final List<IConfigBase> OPTIONS = List.of(
                 ENABLE_WORKBENCH,
@@ -272,6 +277,7 @@ public final class QuickCraftConfigs implements IConfigHandler {
                 ENABLE_ANVIL_RENAME,
                 SHOW_CRAFT_ACTION_BUTTON,
                 DROP_RESULTS_ON_STOP,
+                RETAIN_ONE_CRAFT_INGREDIENT,
                 CRAFT_LOOPS_PER_TICK
         );
 
@@ -891,6 +897,7 @@ public final class QuickCraftConfigs implements IConfigHandler {
                 Crafting.ENABLE_ANVIL_RENAME,
                 Crafting.SHOW_CRAFT_ACTION_BUTTON,
                 Crafting.DROP_RESULTS_ON_STOP,
+                Crafting.RETAIN_ONE_CRAFT_INGREDIENT,
                 ContainerTools.ENABLE_QUICK_TRANSFER,
                 ContainerTools.SHOW_MATCHING_TRANSFER_HIGHLIGHT,
                 ContainerTools.ENABLE_SCROLL_TRANSFER,
@@ -952,6 +959,7 @@ public final class QuickCraftConfigs implements IConfigHandler {
                 Crafting.ENABLE_ANVIL_RENAME,
                 Crafting.SHOW_CRAFT_ACTION_BUTTON,
                 Crafting.DROP_RESULTS_ON_STOP,
+                Crafting.RETAIN_ONE_CRAFT_INGREDIENT,
                 ContainerTools.ENABLE_QUICK_TRANSFER,
                 ContainerTools.ENABLE_SCROLL_TRANSFER,
                 ContainerTools.QUICK_TRANSFER_RETAIN_ONE,
@@ -1044,6 +1052,10 @@ public final class QuickCraftConfigs implements IConfigHandler {
 
     public static boolean isDropCraftResultsOnStopEnabled() {
         return Crafting.DROP_RESULTS_ON_STOP.getBooleanValue();
+    }
+
+    public static boolean isRetainOneCraftIngredientEnabled() {
+        return Crafting.RETAIN_ONE_CRAFT_INGREDIENT.getBooleanValue();
     }
 
     public static boolean isQuickTransferEnabled() {
